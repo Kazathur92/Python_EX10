62051e32ef794de94c4b3e1984924ad3d0ddac93
Alfonso Miranda <Gabmin21@hotmail.com>
2019-01-24 21:17:18 -0600

    deleted ENV file

--- ENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/alfonsomiranda/workspace/python/exercises/DJANGO/PRAC1/ENV/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
